3faf3f499835d92e6ed5d9a50dbc1e9baa3d2e69
topDevelopers123 <jhahimanshu498@gmail.com>
2024-11-30 18:51:17 +0530

    change bg image

--- src/features/User/components/EventModal.jsx
+++ src/features/User/components/EventModal.jsx
@@ -9,15 +9,15 @@ function EventModal({ onClose }) {
         }
     };
   return (
-   <div className="fixed w-[60%] top-[30%] left-[22%] z-50">
-          <div className="bg-white rounded-lg p-6 text-center bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url(https://img.freepik.com/free-photo/small-white-blooming-flowers-pink-background_23-2147752460.jpg)"}} >
+   <div className="fixed w-[50%] top-[30%] left-[25%] z-50">
+          <div className="bg-white rounded-lg p-6 text-center bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url(https://www.shutterstock.com/shutterstock/photos/2011911779/display_1500/stock-vector-pastel-cyan-mint-liquid-marble-watercolor-background-with-gold-lines-and-brush-stains-teal-2011911779.jpg)"}} >
               <div onClick={onClose} className="close w-full flex justify-end text-xl">
                   <TfiClose />
               </div>
             <div className="logo w-full flex justify-center">
                 <img className='w-20' src="./images/logo.png" alt="" />
             </div>
-              <p className="text-sm py-3">If you looking for a lifetime relationship for <strong>yourself or your daughter/son, divorcees/ widows, </strong>and you are too aged and want to get married or anyone else seeking a chance to connect with their life partner. Here, Chat Mangni Pat Byah providing some of the upcoming programs for you. Enroll in this program for lifelong marriage security. The enrollment process is simple just click below on “Enroll Now” to find a wonderful life partner and build strong, meaningful relationships rooted in Vedic rituals. If you are seeking a lifelong partnership, Chat Mangni Pat Byah warmly welcomes you.</p>
+              <p className="text-[15px] py-3">If you looking for a lifetime relationship for <strong>yourself or your daughter/son, divorcees/ widows, </strong>and you are too aged and want to get married or anyone else seeking a chance to connect with their life partner. Here, Chat Mangni Pat Byah providing some of the upcoming programs for you. Enroll in this program for lifelong marriage security. The enrollment process is simple just click below on “Enroll Now” to find a wonderful life partner and build strong, meaningful relationships rooted in Vedic rituals. If you are seeking a lifelong partnership, Chat Mangni Pat Byah warmly welcomes you.</p>
 
               <button onClick={() => {
                   onClose();
